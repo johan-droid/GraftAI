4825cff4908733b0b9cab6392ea23a12a830c8e1
Ashutosh Sahoo <sahooashutosh2022@gmail.com>
2026-04-02 17:13:09 +0530

    refactor: remove unused temp_debug_sqlalchemy.py file and its import error handling

--- backend/temp_debug_sqlalchemy.py
+++
@@ -1,8 +0,0 @@
-import traceback
-try:
-    from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-    from sqlalchemy.pool import NullPool
-    print('imports good')
-except Exception as e:
-    print('import err', type(e), e)
-    traceback.print_exc()
